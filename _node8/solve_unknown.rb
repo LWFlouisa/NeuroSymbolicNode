c29cdf78272be6d8882b4d6d49cf9e4a64e2eab9
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-07-15 00:25:48 -0400

    Update solve_unknown.rb

--- _node8/solve_unknown.rb
+++ _node8/solve_unknown.rb
@@ -2,6 +2,9 @@
 character_fate = File.readlines("_narratives/outcomes/character_fates.txt")
 dating_outcome = File.readlines("_narratives/outcomes/dating_outcomes.txt")
 
+character_fate_rules = File.readlines("_narratives/rulesets/fate_rules.txt")
+dating_outcome_rules = File.readlines("_narratives/rulesets/outcome_rules.txt"
+
 # Make the total imagined branch the size of the darkest path.
 # branch_size = branch_1.size.to_i
 
@@ -15,8 +18,8 @@ open("_imaginedpath/outcomes/nuetral_outcome.txt", "w") { |f|
 
 # Imagined a compromise path that is neither ideal or tragic.
 open("_imaginedpath/peolog/nuetral_outcome.pl", "w") { |f|
-  segment_1 = character_fate[1].strip
-  segment_2 = dating_outcome[0].strip
+  segment_1 = character_fate_rules[1].strip
+  segment_2 = dating_outcome_rules[0].strip
 
   f.puts segment_2
   f.puts "#{segment_1} :- #{segment_2}"
