5ecad892e161778a46e296c74183a305e0a9e205
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-07-15 00:31:30 -0400

    Create nodemachine.rb

---
+++ Brain/nodemachine.rb
@@ -0,0 +1,79 @@
+module Neurosym
+  class StateMachine
+    attr_reader :state
+
+    def initialize
+      @state = "dormant"
+    end
+
+    def activate_node1
+      system("cd _node1; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def activate_node2
+      system("cd _node2; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def activate_node4
+      system("cd _node4; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def activate_node8
+      system("cd _node8; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def activate_node7
+      system("cd _node7; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def activate_node5
+      system("cd _node5; ruby solve_unknown.rb")
+
+      sleep(1)
+    end
+
+    def transition
+      case state
+
+      when "dormant"
+        @state = "node1"
+
+        activate_node1
+      when "node1"
+        @state = "node2"
+
+        activate_node2
+      when "node2"
+        @state = "node4"
+
+        activate_node4
+      when "node4"
+        @state = "node8"
+
+        activate_node8
+      when "node8"
+        @state = "node7"
+
+        activate_node7
+      when "node7"
+        @state = "node5"
+
+        activate_node5
+      when "node5"
+        @state = "dormant"
+
+        puts "State machine is dormant."
+      end
+    end
+  end
+end
